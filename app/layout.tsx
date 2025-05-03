f24a74a4db934e45ed6cc707976424461d14e907
kurkagondex <kurkagondex@gmail.com>
2025-05-04 03:50:07 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,7 +21,7 @@ export const metadata: Metadata = {
     },
     verification: {
         google: 'WpDJBp2DeCYpWL1IWEkO8Im5QdE-Dd11iC6iTs0xLLg',
-        yandex: 'cc2838ce25af1776',
+        yandex: '1774ab9a014da4a4',
     },
 };
 
